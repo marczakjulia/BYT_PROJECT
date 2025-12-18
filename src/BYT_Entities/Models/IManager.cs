c90031e2790441aaab5d995ac863c270ef560747
aniaania321 <s31154@pjwstk.edu.pl>
2025-12-18 12:56:17 +0100

    added interfaces for employee and manager

---
+++ src/BYT_Entities/Models/IManager.cs
@@ -0,0 +1,13 @@
+﻿using BYT_Entities.Enums;
+
+namespace BYT_Entities.Models;
+
+public interface IManager
+{
+    string Department { get; }
+    double BaseSalary { get; }
+    double BonusPercentage { get; }
+
+    double GetBonus();
+    void ChangeToWorker(ShiftType shift, WorkType type, double hourlyRate);
+}
---
+++ src/BYT_Entities/Models/IWorker.cs
@@ -0,0 +1,14 @@
+﻿using BYT_Entities.Enums;
+
+namespace BYT_Entities.Models;
+
+public interface IWorker
+{
+    ShiftType Shift { get; }
+    WorkType TypeOfWork { get; }
+    int HoursWorked { get; }
+    double HourlyRate { get; }
+
+    double CalculateSalary();
+    void ChangeToManager(double baseSalary, double bonusPercentage);
+}
